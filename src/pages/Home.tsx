f2b1aabe884b7cf97954f0e64fccfa2588c906ed
zecazeco <9203689+zecazeco@users.noreply.github.com>
2022-03-09 19:00:40 -0300

    adicionada busca dos valores do bitcoin e ethereum

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,8 +19,15 @@ export default function App() {
   const [error, setError] = useState<string>();
   const [stringIds, setStringIds] = useState<string>();
   const [prices, setPrices] = useState<any>([]);
+  const [priceBTC, setPriceBTC] = useState<string>('0');
+  const [priceETH, setPriceETH] = useState<string>('0');
 
   const loadPrices = async () => {
+    const pricesBase = await fetch( `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=brl` );
+    const dataBase = await pricesBase.json();
+    setPriceBTC(dataBase.bitcoin.brl);
+    setPriceETH(dataBase.ethereum.brl);
+
     const res = await fetch( `https://api.coingecko.com/api/v3/simple/price?ids=${stringIds}&vs_currencies=brl` );
     const data = await res.json();
     setPrices(data);
@@ -28,12 +35,20 @@ export default function App() {
 
   const savePrices = async () => {
     Object.keys(prices).forEach( async (key) => {
+      let id = key;
+      let val = prices[id].brl;
+      let date = new Date();      
+      let relBTC = parseFloat(priceBTC) / parseFloat(val);
+      let relETH = parseFloat(priceETH) / parseFloat(val);
+
       try {
-        let id = key;
-        let date = new Date();
         await setDoc(doc(db, 'portfolio', id, 'prices', date.toISOString()), {
-          data: date.toLocaleDateString("pt-BR"),
-          valor: prices[key].brl,
+          date: date.toLocaleDateString("pt-BR"),
+          price: val,
+          priceBTC: priceBTC,
+          priceETH: priceETH,
+          relBTC: relBTC,
+          relETH: relETH,
         })
       } catch (err) {
         setError('erro');
@@ -41,8 +56,32 @@ export default function App() {
     });
   }
 
+  const calcEverything = async () => {
+    portfolioItems.map((item: any) => {
+      const pricesSnapshot = query(collection(db, 'portfolio', item.id, 'prices'));
+      onSnapshot(pricesSnapshot, (querySnapshot) => {
+        const priceItems = querySnapshot.docs.map(doc => {
+          //console.log(item.id);
+          //console.log(doc.data());
+        });
+      });      
+      
+    });
+/*     const portfolioSnapshot = query(collection(db, 'portfolio'));
+    let strIds = '';
+    onSnapshot(portfolioSnapshot, (querySnapshot) => {
+      const portfolioItems = querySnapshot.docs.map(doc => {
+        strIds += `${doc.data().id},`;
+        return doc.data();
+      });
+      setStringIds(strIds);
+      setPortfolioItems(portfolioItems);
+    }); */
+  }
+
   useEffect(() => {  
     savePrices();
+    calcEverything();
   },[prices]) 
 
 
@@ -51,12 +90,12 @@ export default function App() {
       const portfolioSnapshot = query(collection(db, 'portfolio'));
       let strIds = '';
       onSnapshot(portfolioSnapshot, (querySnapshot) => {
-        const portfolioItems = querySnapshot.docs.map(doc => {
+        const items = querySnapshot.docs.map(doc => {
           strIds += `${doc.data().id},`;
           return doc.data();
         });
         setStringIds(strIds);
-        setPortfolioItems(portfolioItems);
+        setPortfolioItems(items);
       });
     }
     getPortfolio();
