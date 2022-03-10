c37b17804657d2524716bc5ba560c5122c1f3012
zecazeco <9203689+zecazeco@users.noreply.github.com>
2022-03-10 17:14:39 -0300

    teste calculo percentagem da relaçao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,7 +62,8 @@ export default function App() {
 
   useEffect(() => {  
     const calcData = async () => {
-      Object.keys(prices).forEach( async (key) => {
+      console.log('ini refresh');
+      Object.keys(prices).forEach((key) => {
         let id = key;
         let val = prices[id].brl;
         let date = new Date();      
@@ -71,7 +72,7 @@ export default function App() {
 
         //save prices on a collection
         try {
-          await setDoc(doc(db, 'portfolio', id, 'history', date.toISOString()), {
+          setDoc(doc(db, 'portfolio', id, 'history', date.toISOString()), {
             date: date.toLocaleDateString("pt-BR"),
             price: val,
             priceBTC: priceBTC,
@@ -89,9 +90,10 @@ export default function App() {
         if (relBTC > parseFloat(obj.relMaxBTC) || parseFloat(obj.relMaxBTC) == 0) {
           //console.log('é a maior BTC');
           try {
-            await setDoc(doc(db, 'portfolio', obj.id), {
+            setDoc(doc(db, 'portfolio', obj.id), {
               relMaxBTC: relBTC,       
-            },{merge: true})
+            },{merge: true});
+            console.log('1');
           } catch (err) {
             setError('erro2');
           }          
@@ -99,9 +101,10 @@ export default function App() {
         if (relBTC < parseFloat(obj.relMinBTC) || parseFloat(obj.relMinBTC) == 0) {
           //console.log('é a menor BTC');
           try {
-            await setDoc(doc(db, 'portfolio', obj.id), {
+            setDoc(doc(db, 'portfolio', obj.id), {
               relMinBTC: relBTC,       
-            },{merge: true})
+            },{merge: true});
+            console.log('2');
           } catch (err) {
             setError('erro3');
           }            
@@ -109,9 +112,10 @@ export default function App() {
         if (relETH > parseFloat(obj.relMaxETH) || parseFloat(obj.relMaxETH) == 0) {
           //console.log('é a maior ETH');
           try {
-            await setDoc(doc(db, 'portfolio', obj.id), {
+            setDoc(doc(db, 'portfolio', obj.id), {
               relMaxETH: relETH,       
-            },{merge: true})
+            },{merge: true});
+            console.log('3');
           } catch (err) {
             setError('erro4');
           } 
@@ -119,9 +123,10 @@ export default function App() {
         if (relETH < parseFloat(obj.relMinETH) || parseFloat(obj.relMinETH) == 0) {
           //console.log('é a menor ETH');
           try {
-            await setDoc(doc(db, 'portfolio', obj.id), {
+            setDoc(doc(db, 'portfolio', obj.id), {
               relMinETH: relETH,       
-            },{merge: true})
+            },{merge: true});
+            console.log('4');
           } catch (err) {
             setError('erro5');
           }           
@@ -131,19 +136,19 @@ export default function App() {
         let maxMinBTC = (parseFloat(obj.relMaxBTC) - parseFloat(obj.relMinBTC));
         let percBTC = maxMinBTC != 0 ? 100 - (((relBTC - parseFloat(obj.relMinBTC)) * 100) / maxMinBTC) : 100;
         let maxMinETH = (parseFloat(obj.relMaxETH) - parseFloat(obj.relMinETH));
-        //let percETH = 100 - (((relETH - parseFloat(obj.relMinETH)) * 100) / maxMinETH);
         let percETH = maxMinETH != 0 ? 100 - (((relETH - parseFloat(obj.relMinETH)) * 100) / maxMinETH) : 100;        
 
         try {
-          await setDoc(doc(db, 'portfolio', obj.id), {
+          setDoc(doc(db, 'portfolio', obj.id), {
             relPercBTC: percBTC.toFixed(1),
             relPercETH: percETH.toFixed(1),     
-          },{merge: true})
+          },{merge: true});
+          console.log('5');
         } catch (err) {
           setError('erro6');
         }   
       });
-      
+      console.log('end refresh');
     }    
 
     if (isInitialMount.current) {
@@ -168,16 +173,15 @@ export default function App() {
       });
     }
     getPortfolio();
+
   },[]) 
 
 /*   useEffect(() => {  
-    portfolioItems.map((doc: any) => {
-      console.log(doc);
-      console.log(doc.relPercBTC);
-      console.log(doc.relPercETH);
-      console.log(doc.name);
-    });
-  },[portfolioItems])  */
+    function displayTime() {
+      console.log(portfolioItems);
+     }
+    const createClock = setInterval(displayTime, 5000);    
+  },[portfolioItems])   */
 
   return (
     <>
